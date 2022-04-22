c3033aa3aacfb16dd52e34a80716c1b313760885
Alex Dietz <62934780+alex-dietz@users.noreply.github.com>
2022-04-22 10:58:12 +0200

    easy life

--- placard-creator.py
+++ placard-creator.py
@@ -1,24 +1,51 @@
 import copy
-import json
+import random, os,uuid
 from pptx import Presentation
+from pptx.util import Pt
+from pptx.dml.color import RGBColor
+import pandas as pd
 
-
-pres = Presentation("test.pptx")
+pres = Presentation("placards.pptx")
 
 def duplicate_slide(pres, index):
     template = pres.slides[index]
     try:
-        blank_slide_layout = pres.slide_layouts[12]
-    except:
+        blank_slide_layout = pres.slide_layouts[6]
+    except:        
         blank_slide_layout = pres.slide_layouts[len(pres.slide_layouts)-1]
 
     copied_slide = pres.slides.add_slide(blank_slide_layout)
+     # create images dict
+    imgDict = {}
 
+    # now copy contents from external slide, but do not copy slide properties
+    # e.g. slide layouts, etc., because these would produce errors, as diplicate
+    # entries might be generated
     for shp in template.shapes:
-        el = shp.element
-        newel = copy.deepcopy(el)
-        copied_slide.shapes._spTree.insert_element_before(newel, 'p:extLst')   
+        
+        if 'Logo' in shp.name:
+            # save image
 
+            with open(shp.name+'.jpg', 'wb') as f:
+                
+                f.write(shp.image.blob)
+
+            # add image to dict
+            imgDict[shp.name+'.jpg'] = [shp.left, shp.top, shp.width, shp.height]
+        else:
+            # create copy of elem
+            el = shp.element
+            newel = copy.deepcopy(el)
+
+            # add elem to shape tree
+            copied_slide.shapes._spTree.insert_element_before(newel, 'p:extLst')
+
+    # add pictures
+    for k, v in imgDict.items():
+        copied_slide.shapes.add_picture(k, v[0], v[1], v[2], v[3])
+        os.remove(k)
+
+   
     return copied_slide
 
 def find_shape_by_name(shapes, name):
@@ -27,20 +54,30 @@ def find_shape_by_name(shapes, name):
             return shape
     return None
 
-def add_text(shape, text, alignment=None):
-
-    if alignment:
-        shape.vertical_anchor = alignment
-
-    tf = shape.text_frame
-    tf.clear()
-    run = tf.paragraphs[0].add_run()
-    run.text = text if text else ''
 
 
 
-copy = duplicate_slide(pres,0)
-""" slide_title = find_shape_by_name(slide.shapes,'slide_title')
-add_text(slide_title,'TEST SLIDE') """
 
-pres.save('test.pptx')
+shapes = ['Committee','Country','Name']
+delegates = pd.read_excel('delegates.xlsx')
+print(len(delegates['Name']))
+
+for index in range(len(delegates['Name'])):
+    copy_slide = duplicate_slide(pres,0)
+    for field in shapes:
+        
+        shape = find_shape_by_name(copy_slide.shapes,field)
+        text_frame = shape.text_frame
+        text_frame.clear() 
+        p = text_frame.paragraphs[0]
+        run = p.add_run()
+        run.text = delegates[field][index]
+        font = run.font
+        font.name = 'SF Compact Display'
+        font.size = Pt(14)
+        font.italic = None  # cause value to be inherited from theme
+        font.color.rgb = RGBColor(0, 0, 0)
+
+
+
+pres.save('placards.pptx')
